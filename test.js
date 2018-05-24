b2cdd48ca65563ad678756f187187b16d3c22c8c
frozmorn <39112548+frozmorn@users.noreply.github.com>
2018-05-24 19:36:57 +0700

    Update test.js

--- test.js
+++ test.js
@@ -4,7 +4,17 @@ const config = require("./config.json");
 
 client.on("ready", () => {
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-    client.user.setActivity(`Batu,kertas,gunting`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildCreate", guild => {
+    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 client.on("message", (message) => {
     if (message.content === "ayy") {
