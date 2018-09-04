e0f232bcdec0332f71c0828ac6018d4e5387525a
frozmorn <39112548+frozmorn@users.noreply.github.com>
2018-09-05 00:57:29 +0700

    Update test.js

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const config = require("./config.json");
 
 client.on("ready", () => {
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-    client.user.setActivity(`DINUS Inside 2018`);
+    client.user.setActivity(`DINUS Inside 2018 | ft.Virzha`);
 });
 client.on("message", (message) => {
     if (message.content === "ayy") {
